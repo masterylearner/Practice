b334e40e3b9d1d9a1f20b10c5c123635e0b42e1a
masterylearner <masterylearner727@gmail.com>
2025-11-05 18:14:25 +0530

    this was test

--- Test/src/main/java/com/example/test/Service/Doc_Interface.java
+++
@@ -1,9 +0,0 @@
-package com.example.test.Service;
-
-public interface Doc_Interface {
-
-    public void DB_hit();
-    public void process();
-    public String print();
-    public String callMethod();
-}
